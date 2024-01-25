7950436a2976a6efe8c4882e31e5df7c14c85e0b
maxefimov <efimov.top.academy@gmail.com>
2024-01-25 20:55:15 +0300

    25-01-24

--- NetFileStreamApp/Program.cs
+++ NetFileStreamApp/Program.cs
@@ -5,24 +5,24 @@ string fileName = @"file01.dat";
 
 string text = @"Закрывает текущий поток и отключает все ресурсы (например, сокеты и файловые дескрипторы), связанные с текущим потоком. Вместо вызова данного метода, убедитесь в том, что поток надлежащим образом ликвидирован.\r\n\r\n(Унаследовано от Stream)";
 
-//using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
-//{
-//    Console.WriteLine(fileStream.Name);
-//    Console.WriteLine(fileStream.Length);
-//    Console.WriteLine(fileStream.Position);
+using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+{
+    Console.WriteLine(fileStream.Name);
+    Console.WriteLine(fileStream.Length);
+    Console.WriteLine(fileStream.Position);
 
-//    //fileStream.Write(Encoding.UTF8.GetBytes(text));
-//    byte[] buffer = new byte[10];
+    //fileStream.Write(Encoding.UTF8.GetBytes(text));
+    byte[] buffer = new byte[10];
 
-//    while(fileStream.Read(buffer, 0, buffer.Length - 1) != 0)
-//    {
-//        //fileStream.Read(buffer, 0, buffer.Length);
-//        Console.Write(Encoding.Default.GetString(buffer));
-//        Array.Clear(buffer);
-//    }
+    while (fileStream.Read(buffer, 0, buffer.Length - 1) != 0)
+    {
+        //fileStream.Read(buffer, 0, buffer.Length);
+        Console.Write(Encoding.Default.GetString(buffer));
+        Array.Clear(buffer);
+    }
 
 
-//}
+}
 
 
 using (StreamWriter writer = new(fileName))
